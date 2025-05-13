f9ba8d0d460ea4c4a0db14b7b3e18058121336c1
uncenter <uncenter@uncenter.dev>
2025-05-13 12:35:48 -0400

    use hyperfine under the hood

--- src/generator.ts
+++ src/generator.ts
@@ -2,14 +2,14 @@ import { ensureDir, expandGlob } from "https://deno.land/std@0.98.0/fs/mod.ts";
 import { join } from "https://deno.land/std@0.98.0/path/mod.ts";
 import { article, sentence } from "./lorem-ipsum.ts";
 
-export default async function generate(path, numPages = 1) {
+export default async function generate(path: string, pages: number) {
   await ensureDir(path);
 
   for await (const file of expandGlob("*.md", { root: path })) {
     await Deno.remove(file.path);
   }
 
-  for (let index = 1; index <= numPages; index++) {
+  for (let index = 1; index <= pages; index++) {
     const file = join(path, `page-${index}.md`);
     const permalink = `pages/${index}.html`;
     const content = generateContent(permalink);
@@ -18,7 +18,7 @@ export default async function generate(path, numPages = 1) {
   }
 }
 
-function generateContent(permalink) {
+function generateContent(permalink: string) {
   const title = sentence();
   const text = article();
 
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,128 @@
-import { ITERATIONS, PAGE_VARIATIONS, RUNNERS } from "./config.ts";
-import { Bench } from "tinybench";
+import { join } from "https://deno.land/std@0.98.0/path/mod.ts";
+import generate from "./generator.ts";
+import spawn from "nano-spawn";
 
-for (const pages of PAGE_VARIATIONS) {
-  const bench = new Bench({ name: pages + " pages", iterations: ITERATIONS });
+type Generator = {
+  name: string;
+  build: string;
+  content: string;
+  output: string;
+};
 
-  for (const runner of RUNNERS) {
-    bench.add(runner.name, async () => {
-      await runner.build();
-    }, {
-      beforeEach: async () => {
-        await runner.clear();
-      },
-      beforeAll: async () => {
-        await runner.generate(pages);
-      },
+const GENERATORS: Array<Generator> = [
+  {
+    name: "lume",
+    build: "deno task build",
+    content: "pages",
+    output: "_site",
+  },
+  {
+    name: "eleventy",
+    build: "node ./node_modules/.bin/eleventy",
+    content: "pages",
+    output: "_site",
+  },
+    {
+      name: "jekyll",
+      build:"bundle exec jekyll build",
+      content: "_pages",
+      output: "_site",
+    },
+    {
+      name: "hugo",
+      build:"hugo -D",
+      content: "content/pages",
+      output: "public",
+    },
+];
+
+const tempFilePath = await Deno.makeTempFile();
+
+for (
+  const pages of [10, 100, 1000, 10000]
+) {
+  const results: Array<HyperfineResult> = [];
+
+  for (const runner of GENERATORS) {
+    const cwd = join(Deno.cwd(), "test", runner.name);
+    const contentDir = join(cwd, runner.content);
+    const outputDir = join(cwd, runner.output);
+
+    await generate(contentDir, pages);
+
+    const cmd = await spawn("hyperfine", [
+      "--warmup",
+      "1",
+      "--prepare",
+      `rm -rf ${outputDir}`,
+      "--export-json",
+      tempFilePath,
+      "--style",
+      "none",
+      "-n",
+      runner.name,
+      runner.build,
+    ], {
+      cwd,
     });
+
+    console.log(cmd.output);
+    const data = await Deno.readTextFile(tempFilePath);
+    results.push(
+      (JSON.parse(data) as { results: Array<HyperfineResult> })
+        .results[0],
+    );
   }
 
-  await bench.run();
+  console.log(`${pages} pages`);
+  console.log(formatResultsToMarkdown(results));
+}
 
-  console.log(bench.name);
-  console.table(bench.table(({ name, result }) => {
-    const { latency } = result!;
+type HyperfineResult = {
+  command: string;
+  mean: number;
+  stddev: number;
+  median: number;
+  system: number;
+  min: number;
+  max: number;
+  times: Array<number>;
+  exit_codes: Array<number>;
+};
 
-    return {
-      "Name": name,
-      "Mean (ms)": `${latency.mean.toFixed(2)} \xb1 ${latency.rme.toFixed(2)}%`,
-      "Median (ms)": `${latency.p50!.toFixed(2)} \xb1 ${
-        latency.mad!.toFixed(2)
-      }`,
-      "Samples": latency.samples.length,
-    };
-  }));
+function formatResultsToMarkdown(results: Array<HyperfineResult>): string {
+  const fastest = results.reduce((a, b) => (a.mean < b.mean ? a : b));
+
+  const cols = [
+    "command",
+    "mean (s)",
+    "median (s)",
+    "min (s)",
+    "max (s)",
+    "relative",
+  ];
+  const header = `| ${cols.join(" | ")} |\n${"|---".repeat(cols.length)}|`;
+
+  const rows = results.map((result) => {
+    const isFastest = result === fastest;
+    const rel = isFastest
+      ? "-"
+      : `${
+        (((result.mean - fastest.mean) / fastest.mean) * 100).toFixed(0)
+      }% slower`;
+    const name = isFastest ? `${result.command} 🏆` : result.command;
+
+    return `| ${
+      [
+        name,
+        result.mean.toFixed(3),
+        result.median.toFixed(3),
+        result.min.toFixed(3),
+        result.max.toFixed(3),
+        rel,
+      ].join(" | ")
+    } |`;
+  });
+
+  return [header, ...rows].join("\n");
 }
